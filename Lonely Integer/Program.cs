c70ab10799fffd1f6aae6c9160ce5491de6d81df
ElhamDrg <elham.dargi@gmail.com>
2022-07-06 21:57:53 +0200

    Add project files.

---
+++ Lonely Integer/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lonely_Integer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> array = new List<int> { 1,2,3,4,3,2,1};
+            int result = 0;
+            foreach (var item in array)
+            {
+                if (array.Count(i => i==item)==1)
+                {
+                    result = item;
+                }             
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
